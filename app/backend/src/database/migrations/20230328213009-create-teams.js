8b8f0453871ab58a11ca55a18939fbb5eaa97c6e
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-28 19:02:40 -0300

    feat: migration create teams

---
+++ app/backend/src/database/migrations/20230328213009-create-teams.js
@@ -0,0 +1,23 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      teamName: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+        field: 'team_name',
+      }, 
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+
+  }
+};
