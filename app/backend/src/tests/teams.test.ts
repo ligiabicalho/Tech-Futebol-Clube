6a01a131bb6fb010313b4f408db430a5741ef302
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-30 16:37:40 -0300

    test: fix tests GET teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -16,59 +16,49 @@ const { expect } = chai;
 
 describe('Verifica rota /teams', () => {
   let chaiHttpResponse: Response;
-  describe('Verifica método GET no endpoint /teams', () => {
-
-    beforeEach(async () => {
-      sinon
-        .stub(Team, "findAll") // getAll na Sequelize é findAll!
-        .resolves(allTeams as Team[]); // a própria Model serve como tipo.
-    });
-
-    afterEach(() => {
-      (Team.findAll as sinon.SinonStub).restore();
+  afterEach(() => {
+      sinon.restore();
     })
-  
+  describe('Verifica método GET no endpoint /teams', () => {    
     it('retorna status 200 e a lista completa de teams', async () => {
-    
+      sinon
+        .stub(Team, "findAll")
+        .resolves(allTeams as Team[]);
+
       chaiHttpResponse = await chai
         .request(app)
         .get('/teams');
     
-      expect(chaiHttpResponse.status).to.be.equals(200);
+      expect(chaiHttpResponse.status).to.be.equal(200);
       expect(chaiHttpResponse.body).to.deep.equal(allTeams);
 
     });
   });
-  describe('Verifica método GET no endpoint /teams:id', () => {
-
-    beforeEach(async () => {
+  describe('Verifica método GET no endpoint /teams:id', () => { 
+    it('retorna status 200 e dados de um time específico', async () => {
       sinon
-        .stub(Team, "findByPk") // getAll na Sequelize é findAll!
-        .resolves(allTeams[0] as Team); // a própria Model serve como tipo.
-    });
-
-    afterEach(() => {
-      (Team.findByPk as sinon.SinonStub).restore();
-    })
-  
-    it('retorna status 200 e dados de um time especifico', async () => {
+        .stub(Team, "findByPk")
+        .resolves(allTeams[0] as Team);
     
       chaiHttpResponse = await chai
         .request(app)
-        .get('/teams:1');
+        .get('/teams/:1');
     
-      expect(chaiHttpResponse.status).to.be.equals(200);
+      expect(chaiHttpResponse.status).to.be.equal(200);
       expect(chaiHttpResponse.body).to.deep.equal(allTeams[0]);    
     });
 
     it('retorna status 404 quando id não corresponde a time cadastrado', async () => {
-    
+      sinon
+        .stub(Team, "findByPk")
+        .resolves(null);
+
       chaiHttpResponse = await chai
         .request(app)
-        .get('/teams:99');
-      const messageNotFound = 'Team does not exist';
-      expect(chaiHttpResponse.status).to.be.equals(404);
-      expect(chaiHttpResponse.body).to.deep.equal({messageNotFound});    
+        .get('/teams/:99');
+      const messageNotFound = { error: 'Team does not exist in database!'};
+      expect(chaiHttpResponse.status).to.be.equal(404);
+      expect(chaiHttpResponse.body).to.deep.equal(messageNotFound);    
     });
   });
 });
