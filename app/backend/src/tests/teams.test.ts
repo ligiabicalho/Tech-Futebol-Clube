29b126764fb004f9f0380579cdea53e6bacfe9f2
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-30 11:14:56 -0300

    test: TDD - verifica endpoint /teams:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -5,7 +5,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Team from '../database/models/TeamModel';
-// import { ITeam } from '../interfaces/ITeams';
+// import ITeam from '../interfaces/ITeams';
 import { allTeams } from './mocks/teamsMock';
 
 import { Response } from 'superagent';
@@ -14,27 +14,62 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Verifica método GET em /teams', () => {
+describe('Verifica rota /teams', () => {
   let chaiHttpResponse: Response;
+  describe('Verifica método GET no endpoint /teams', () => {
 
-  beforeEach(async () => {
-    sinon
-      .stub(Team, "findAll") // getAll na Sequelize é findAll!
-      .resolves(allTeams as Team[]); // a própria Model serve como tipo.
-  });
+    beforeEach(async () => {
+      sinon
+        .stub(Team, "findAll") // getAll na Sequelize é findAll!
+        .resolves(allTeams as Team[]); // a própria Model serve como tipo.
+    });
 
-  afterEach(() => {
-    (Team.findAll as sinon.SinonStub).restore();
-  })
- 
-  it('retorna a lista completa de teams', async () => {
+    afterEach(() => {
+      (Team.findAll as sinon.SinonStub).restore();
+    })
   
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/teams');
-   
-    expect(chaiHttpResponse.status).to.be.equals(200);
-    expect(chaiHttpResponse.body).to.deep.equal(allTeams);
+    it('retorna status 200 e a lista completa de teams', async () => {
+    
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams');
+    
+      expect(chaiHttpResponse.status).to.be.equals(200);
+      expect(chaiHttpResponse.body).to.deep.equal(allTeams);
 
+    });
+  });
+  describe('Verifica método GET no endpoint /teams:id', () => {
+
+    beforeEach(async () => {
+      sinon
+        .stub(Team, "findByPk") // getAll na Sequelize é findAll!
+        .resolves(allTeams[0] as Team); // a própria Model serve como tipo.
+    });
+
+    afterEach(() => {
+      (Team.findByPk as sinon.SinonStub).restore();
+    })
+  
+    it('retorna status 200 e dados de um time especifico', async () => {
+    
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams:1');
+    
+      expect(chaiHttpResponse.status).to.be.equals(200);
+      expect(chaiHttpResponse.body).to.deep.equal(allTeams[0]);    
+    });
+
+    it('retorna status 404 quando id não corresponde a time cadastrado', async () => {
+    
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams:99');
+      const messageNotFound = 'Team does not exist';
+      expect(chaiHttpResponse.status).to.be.equals(404);
+      expect(chaiHttpResponse.body).to.deep.equal({messageNotFound});    
+    });
   });
 });
+
