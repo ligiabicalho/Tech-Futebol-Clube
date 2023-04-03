3fd2bcba20cc61e16d16b57006d0801eb2b19430
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-03 20:58:05 -0300

    test: TDD - Verifica rota get  /matches

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,38 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Match from '../database/models/MatchModel';
+import { Response } from 'superagent';
+import { allMatches, MatchesRes} from './mocks/matchesMock';
+// import { IMatch, MatchRes } from '../interfaces/IMatch';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Verifica rota /matches', () => {
+  let chaiHttpResponse: Response;
+
+  afterEach(() => {
+      sinon.restore();
+    })
+  
+ describe('Verifica método GET no endpoint /matches', () => {    
+    it('retorna status 200 e a lista completa de partidas', async () => {
+      sinon
+        .stub(Match, "findAll")
+        .resolves(allMatches as Match[]);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matches');
+    
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.deep.equal(MatchesRes);
+
+    });
+  });
+});
---
+++ app/backend/src/tests/mocks/matchesMock.ts
@@ -0,0 +1,51 @@
+const allMatches = [
+  {
+    "id": 1,
+    "homeTeamId": 16,
+    "homeTeamGoals": 1,
+    "awayTeamId": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+  },
+  {
+    "id": 2,
+    "homeTeamId": 16,
+    "homeTeamGoals": 2,
+    "awayTeamId": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+  }
+]
+
+const MatchesRes = [
+  {
+    "id": 1,
+    "homeTeamId": 16,
+    "homeTeamGoals": 1,
+    "awayTeamId": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Grêmio"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeamId": 16,
+    "homeTeamGoals": 2,
+    "awayTeamId": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Internacional"
+    }
+  }
+]
+
+export { allMatches, MatchesRes }; 
