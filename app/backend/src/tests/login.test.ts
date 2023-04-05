97858a2e971f61b7c1fae22650fa11c95dfd8f03
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-05 14:29:52 -0300

    test: Verifica rota GET /login/role

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -8,7 +8,8 @@ import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcryptjs';
 import User from '../database/models/UserModel';
 import { Response } from 'superagent';
-import { payload, token, user, userValid } from './mocks/loginMock';
+import { token, user, userValid } from './mocks/loginMock';
+import statusCodes from '../utils/statusCode';
 
 chai.use(chaiHttp);
 
@@ -21,7 +22,7 @@ describe('Verifica rota /login', () => {
       sinon.restore();
     })
   
-  describe('Acessa com sucesso método POST no endpoint /login', () => {    
+  describe('POST no endpoint /login com sucesso', () => {    
     it('retorna status 200 e o token', async () => {
       sinon
         .stub(User, "findOne").resolves(user);
@@ -33,18 +34,18 @@ describe('Verifica rota /login', () => {
       chaiHttpResponse = await chai
         .request(app).post('/login').send(userValid);
     
-      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.status).to.be.equal(statusCodes.OK);
       expect(chaiHttpResponse.body).to.deep.equal({token});
     });
   });
-  describe('Acesso negado método POST no endpoint /login', () => {
+  describe('POST no endpoint /login sem sucesso', () => {
     const messageError = { message: 'All fields must be filled' }
 
     it('Quando email não é informado, retorna status 400', async () => {
       chaiHttpResponse = await chai
         .request(app).post('/login').send(userValid.password);
     
-      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.status).to.be.equal(statusCodes.BAD_REQUEST);
       expect(chaiHttpResponse.body).to.deep.equal(messageError);    
     });
 
@@ -52,8 +53,34 @@ describe('Verifica rota /login', () => {
       chaiHttpResponse = await chai
         .request(app).post('/login').send(userValid.email);
       const messageNotFound = { error: 'Team does not exist in database!'};
-      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.status).to.be.equal(statusCodes.BAD_REQUEST);
       expect(chaiHttpResponse.body).to.deep.equal(messageError);  
+    });
+  });
+  describe('GET no endpoint /login/role', () => {
+    it('Com token válido, retorna `role` do usuário, status 200', async () => {
+      const responseLogin = await chai.request(app)
+        .post('/login')
+        .send(userValid);
+      const { token } = responseLogin.body;
+
+      chaiHttpResponse = await chai.request(app)
+        .get('/login/role')
+        .send(userValid)
+        .set('Authorization', `${token}`);
+
+      expect(chaiHttpResponse.status).to.be.equal(statusCodes.OK);
+      expect(chaiHttpResponse.body).to.deep.equal({ role: user.role });    
+    });
+
+    it('Sem token, acesso não autorizado na rota, status 401', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/login/role')
+        .send(userValid)
+        .set('Authorization', '');
+    
+      expect(chaiHttpResponse.status).to.be.equal(statusCodes.UNAUTHORIZED);
+      expect(chaiHttpResponse.body).to.deep.equal({ message: 'Token not found' }); 
     });  
   });
 });
